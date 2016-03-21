bffa73b18272817f9a542256f28a621ca52e7401
Prashant <prashantgeorge36@gmail.com>
2016-03-21 11:42:32 -0700

    Added support for Kitchen tests. Added flag to not setup group permissions when running test so that it passes.

---
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'ansible-mongodb ::default' do
+
+  describe package('mongodb-org-server') do
+    it { should be_installed.by('apt') }
+  end
+
+    describe package('mongodb-org-shell') do
+    it { should be_installed.by('apt') }
+  end
+
+end
